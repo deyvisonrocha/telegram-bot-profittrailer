f160642f0686754730671514e45e824de8603746
Kelvin Steiner Santos <kelvinsteinersantos@gmail.com>
2018-08-28 08:31:51 -0300

    adicionando PT_HOST no .env.example e pequenas modificações

--- src/functions/utils.js
+++ src/functions/utils.js
@@ -1,13 +1,21 @@
-const totalBalance = resultApi => {
-  return parseFloat(resultApi.realBalance) + parseFloat(resultApi.totalDCACurrentValue) + parseFloat(resultApi.totalPairsCurrentValue) + parseFloat(resultApi.totalPendingCurrentValue) + parseFloat(resultApi.totalDustCurrentValue)
-}
 
-const totalBinanceCoin = resultApi => {
-  return parseFloat(resultApi.watchModeLogData[0].averageCalculator.totalAmount)
-}
+const propsBalance = [
+  "realBalance",
+  "totalDCACurrentValue",
+  "totalPairsCurrentValue",
+  "totalPendingCurrentValue",
+  "totalDustCurrentValue"
+]
+
+const totalBalance = (resultApi) =>
+  propsBalance
+    .map(name => parseFloat(resultApi[name]))
+    .reduce((a,b) => a+b)
+
+const totalBinanceCoin = (resultApi) =>
+  parseFloat(resultApi.watchModeLogData[0].averageCalculator.totalAmount)
 
 module.exports = {
   totalBalance: totalBalance,
   totalBinanceCoin: totalBinanceCoin
 }
-
