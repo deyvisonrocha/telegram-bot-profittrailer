1b8a473ae7683108ada7cde2ae11fe2f9dc6a26e
Kelvin Steiner Santos <kelvinsteinersantos@gmail.com>
2018-08-28 11:40:58 -0300

    pega username do servidor do telegram

--- index.js
+++ index.js
@@ -6,5 +6,16 @@ const bot = require('./src/services/telegram')
 const saldo = require('./src/commands/saldo')
 const bags = require('./src/commands/bags')
 
-bot.startPolling()
-logger.info('Telegram polling')
+// bot.startPolling()
+// logger.info('Telegram polling')
+
+bot.telegram.getMe()
+  .then((botInfo) => {
+    logger.info(`Bot username: @${botInfo.username}`)
+    bot.options.username = botInfo.username
+    return botInfo
+  }).then((botInfo) => {
+    bot.startPolling()
+    logger.info('Telegram polling')
+    return botInfo
+  })
