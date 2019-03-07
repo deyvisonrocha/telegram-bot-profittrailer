a0db9578e3f875ad80bfc26c1a681e85270201f2
Deyvison Rocha <deyvison@gmail.com>
2019-03-07 13:34:08 -0300

    remoção de funções que não estão sendo utilizadas

--- src/functions/utils.js
+++ src/functions/utils.js
@@ -1,29 +1,3 @@
-const BNB_NAME = 'BNB'
-
-const propsBalance = [
-  'realBalance',
-  'totalDCACurrentValue',
-  'totalPairsCurrentValue',
-  'totalPendingCurrentValue',
-  'totalDustCurrentValue'
-]
-
-const totalBalance = (resultApi) =>
-  propsBalance
-    .map(name => parseFloat(resultApi[name]))
-    .reduce((a, b) => a + b)
-
-const totalBinanceCoin = (resultApi) => {
-  var bnbObjs =
-    resultApi.watchModeLogData.filter(obj => obj.currency === BNB_NAME)
-  var obj = bnbObjs[0]
-  // Caso não encontre BNB no watchmode
-  if (obj == null) {
-    return null
-  }
-  return parseFloat(obj.averageCalculator.totalAmount)
-}
-
 const marketColumn = () => {
   return 'Moeda'.leftJustify(7, ' ')
 }
@@ -33,8 +7,6 @@ const marketRows = (market) => {
 }
 
 module.exports = {
-  totalBalance: totalBalance,
-  totalBinanceCoin: totalBinanceCoin,
   marketColumn,
   marketRows
 }
